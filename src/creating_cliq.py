97c4c07314fa1b6547cdfcd9a0799aa6e0e65f2b
Bell Codo <bellcodo@gmail.com>
2016-09-27 07:56:52 -0700

    Create creating_cliq.py
    Initial commit after removing unnecessary parts

---
+++ src/creating_cliq.py
@@ -0,0 +1,11 @@
+import simplegui
+
+def draw(canvas):
+    pass
+
+# Create a frame and assign callbacks to event handlers
+frame = simplegui.create_frame("Home", 500, 500)
+frame.set_draw_handler(draw)
+
+# Start the frame animation
+frame.start()
